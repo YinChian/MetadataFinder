77b3e0add2d98278b89f76b02246508875d90924
YinChian <82175095+YinChian@users.noreply.github.com>
2024-04-12 02:59:48 +0800

    剩下轉出Word文件的部分

--- MetadataFinder/Form1.Designer.cs
+++ MetadataFinder/Form1.Designer.cs
@@ -32,18 +32,21 @@
             ExportTXTButton = new Button();
             ExportWordButton = new Button();
             OutputBox = new TextBox();
-            statusStrip = new StatusStrip();
-            StatusLabel = new ToolStripStatusLabel();
             folderBrowserDialog = new FolderBrowserDialog();
             FocalSelector = new ComboBox();
-            statusStrip.SuspendLayout();
+            RefreshButton = new Button();
+            saveFileDialog = new SaveFileDialog();
+            ColumNumPickup = new NumericUpDown();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)ColumNumPickup).BeginInit();
             SuspendLayout();
             // 
             // OpenFileButton
             // 
-            OpenFileButton.Location = new Point(12, 12);
+            OpenFileButton.Location = new Point(15, 15);
+            OpenFileButton.Margin = new Padding(4);
             OpenFileButton.Name = "OpenFileButton";
-            OpenFileButton.Size = new Size(118, 23);
+            OpenFileButton.Size = new Size(437, 29);
             OpenFileButton.TabIndex = 0;
             OpenFileButton.Text = "開啟資料夾...";
             OpenFileButton.UseVisualStyleBackColor = true;
@@ -51,46 +54,39 @@
             // 
             // ExportTXTButton
             // 
-            ExportTXTButton.Location = new Point(225, 415);
+            ExportTXTButton.Enabled = false;
+            ExportTXTButton.Location = new Point(289, 526);
+            ExportTXTButton.Margin = new Padding(4);
             ExportTXTButton.Name = "ExportTXTButton";
-            ExportTXTButton.Size = new Size(127, 23);
+            ExportTXTButton.Size = new Size(163, 29);
             ExportTXTButton.TabIndex = 1;
             ExportTXTButton.Text = "轉出文字檔";
             ExportTXTButton.UseVisualStyleBackColor = true;
+            ExportTXTButton.Click += ExportTXTButton_Click;
             // 
             // ExportWordButton
             // 
-            ExportWordButton.Location = new Point(358, 415);
+            ExportWordButton.Location = new Point(460, 526);
+            ExportWordButton.Margin = new Padding(4);
             ExportWordButton.Name = "ExportWordButton";
-            ExportWordButton.Size = new Size(127, 23);
+            ExportWordButton.Size = new Size(163, 29);
             ExportWordButton.TabIndex = 2;
             ExportWordButton.Text = "轉出Word檔";
             ExportWordButton.UseVisualStyleBackColor = true;
+            ExportWordButton.Click += ExportWordButton_Click;
             // 
             // OutputBox
             // 
+            OutputBox.AcceptsReturn = true;
             OutputBox.BorderStyle = BorderStyle.FixedSingle;
-            OutputBox.Location = new Point(12, 41);
+            OutputBox.Location = new Point(15, 52);
+            OutputBox.Margin = new Padding(4);
             OutputBox.Multiline = true;
             OutputBox.Name = "OutputBox";
             OutputBox.ReadOnly = true;
-            OutputBox.Size = new Size(473, 368);
+            OutputBox.ScrollBars = ScrollBars.Both;
+            OutputBox.Size = new Size(608, 466);
             OutputBox.TabIndex = 3;
-            // 
-            // statusStrip
-            // 
-            statusStrip.Items.AddRange(new ToolStripItem[] { StatusLabel });
-            statusStrip.Location = new Point(0, 446);
-            statusStrip.Name = "statusStrip";
-            statusStrip.Size = new Size(497, 22);
-            statusStrip.TabIndex = 4;
-            statusStrip.Text = "statusStrip1";
-            // 
-            // StatusLabel
-            // 
-            StatusLabel.Name = "StatusLabel";
-            StatusLabel.Size = new Size(115, 17);
-            StatusLabel.Text = "請先開啟圖檔資料夾";
             // 
             // FocalSelector
             // 
@@ -98,28 +94,61 @@
             FocalSelector.FlatStyle = FlatStyle.System;
             FocalSelector.FormattingEnabled = true;
             FocalSelector.Items.AddRange(new object[] { "全片幅 (不使用等校焦段)", "Nikon APS-C 片幅 (1.5倍)", "Nikon CX 片幅 (1.7倍)", "Canon APS-H 片幅 (1.3倍)", "Canon APS-C 片幅 (1.6倍)", "Sony APS-C 片幅 (1.5倍)" });
-            FocalSelector.Location = new Point(12, 415);
+            FocalSelector.Location = new Point(13, 528);
+            FocalSelector.Margin = new Padding(4);
             FocalSelector.Name = "FocalSelector";
-            FocalSelector.Size = new Size(207, 23);
+            FocalSelector.Size = new Size(268, 27);
             FocalSelector.TabIndex = 5;
+            // 
+            // RefreshButton
+            // 
+            RefreshButton.Location = new Point(460, 15);
+            RefreshButton.Margin = new Padding(4);
+            RefreshButton.Name = "RefreshButton";
+            RefreshButton.Size = new Size(163, 29);
+            RefreshButton.TabIndex = 6;
+            RefreshButton.Text = "重新讀取";
+            RefreshButton.UseVisualStyleBackColor = true;
+            RefreshButton.Click += RefreshButton_Click;
+            // 
+            // ColumNumPickup
+            // 
+            ColumNumPickup.Location = new Point(505, 563);
+            ColumNumPickup.Maximum = new decimal(new int[] { 5, 0, 0, 0 });
+            ColumNumPickup.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            ColumNumPickup.Name = "ColumNumPickup";
+            ColumNumPickup.Size = new Size(118, 27);
+            ColumNumPickup.TabIndex = 7;
+            ColumNumPickup.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(460, 565);
+            label1.Name = "label1";
+            label1.Size = new Size(39, 19);
+            label1.TabIndex = 8;
+            label1.Text = "欄數";
             // 
             // MainWindow
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(9F, 19F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(497, 468);
+            ClientSize = new Size(639, 602);
+            Controls.Add(label1);
+            Controls.Add(ColumNumPickup);
+            Controls.Add(RefreshButton);
             Controls.Add(FocalSelector);
-            Controls.Add(statusStrip);
             Controls.Add(OutputBox);
             Controls.Add(ExportWordButton);
             Controls.Add(ExportTXTButton);
             Controls.Add(OpenFileButton);
+            Margin = new Padding(4);
             MaximizeBox = false;
             Name = "MainWindow";
             Text = "Form1";
             Load += MainWindow_Load;
-            statusStrip.ResumeLayout(false);
-            statusStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)ColumNumPickup).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -130,9 +159,11 @@
         private Button ExportTXTButton;
         private Button ExportWordButton;
         private TextBox OutputBox;
-        private StatusStrip statusStrip;
-        private ToolStripStatusLabel StatusLabel;
         private FolderBrowserDialog folderBrowserDialog;
         private ComboBox FocalSelector;
+        private Button RefreshButton;
+        private SaveFileDialog saveFileDialog;
+        private NumericUpDown ColumNumPickup;
+        private Label label1;
     }
 }
--- MetadataFinder/Form1.cs
+++ MetadataFinder/Form1.cs
@@ -1,53 +1,124 @@
 using System.Collections.Generic;
+using System.Drawing.Imaging;
 using System.Text;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Office2021.MipLabelMetaData;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
 
 namespace MetadataFinder
 {
     public partial class MainWindow : Form
     {
-        private string imgdir = "";
-        private readonly Dictionary<string, int> exifConst = new Dictionary<string, int>() {
-            {"ExposureTime", 0x829A},
-            {"FNumber", 0x829D},
-            {"ISOSpeed", 0x8827},
-            {"FocalLength", 0x920A}
-        };
+        private string _imageLoadingDir = "";
+        private bool _isTouched = false;
+
+        private void ImageDataFetchProc()
+        {
+            Invoke(new MethodInvoker(delegate() { OutputBox.Text = @"Test";}));
+            //OutputBox.Clear();
+
+            var imageFiles = Directory.GetFiles(_imageLoadingDir, "*.jpg");
+
+            foreach (var imagePath in imageFiles)
+            {
+                OutputBox.Text += imagePath;
+                OutputBox.Text += Environment.NewLine;
+
+                try
+                {
+                    var image = Image.FromFile(imagePath);
+                    //statusStrip.Text = @"處理中";
+                    foreach (var propertyItem in image.PropertyItems)
+                    {
+                        switch (propertyItem.Id)
+                        {
+                            // 焦段
+                            case 0x920A:
+                                {
+                                    var focalLength = BitConverter.ToInt32(propertyItem.Value, 0);
+
+                                    OutputBox.Text += $@"焦段: {focalLength} mm";
+
+                                    if (FocalSelector.SelectedIndex != 0)
+                                    {
+                                        double[] focal = { 1.5, 1.7, 1.3, 1.6, 1.5 };
+                                        OutputBox.Text += $@" (等校焦段: {focalLength * focal[FocalSelector.SelectedIndex - 1]})";
+                                    }
+
+                                    OutputBox.Text += Environment.NewLine;
+                                    break;
+                                }
+
+                            // ISO值
+                            case 0x8827:
+                                {
+                                    var iso = BitConverter.ToUInt16(propertyItem.Value, 0);
+
+                                    OutputBox.Text += $@"ISO: {iso}";
+                                    OutputBox.Text += Environment.NewLine;
+                                    break;
+                                }
+
+                            // 曝光時間
+                            case 0x829A:
+                                {
+                                    var numerator = BitConverter.ToUInt32(propertyItem.Value, 0);
+                                    var denominator = BitConverter.ToUInt32(propertyItem.Value, 4);
+
+                                    if (denominator == 1 || denominator == 10)
+                                    {
+                                        OutputBox.Text += $@"曝光時間: {((double)numerator / denominator)} 秒";
+                                    }
+                                    else
+                                    {
+                                        OutputBox.Text += $@"曝光時間: {numerator}/{denominator} 秒";
+                                    }
+                                    OutputBox.Text += Environment.NewLine;
+                                    break;
+                                }
+
+                            // 光圈
+                            case 0x829D:
+                                {
+                                    var numerator = BitConverter.ToUInt32(propertyItem.Value, 0);
+                                    var denominator = BitConverter.ToUInt32(propertyItem.Value, 4);
+                                    var fStop = (double)numerator / denominator;
+                                    OutputBox.Text += $@"光圈: f/{fStop}";
+                                    OutputBox.Text += Environment.NewLine;
+                                    break;
+                                }
+                        }
+                    }
+                    OutputBox.Text += Environment.NewLine;
+                    ExportTXTButton.Enabled = true;
+                    ExportWordButton.Enabled = true;
+                    _isTouched = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($@"無法處理檔案！原因：{ex.Message}", @"錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
+
         private void OpenFileButton_Click(object sender, EventArgs e)
         {
             if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
             {
-                imgdir = folderBrowserDialog.SelectedPath;
+                _imageLoadingDir = folderBrowserDialog.SelectedPath;
             }
             else return;
-
-            OutputBox.Clear();
-
-            string[] imageFiles = Directory.GetFiles(imgdir, "*.jpg");
-
-            foreach (string imagePath in imageFiles)
-            {
-                try
-                {
-                    Image image = Image.FromFile(imagePath);
-                    foreach (KeyValuePair<string, int> entry in exifConst)
-                    {
-                        // do something with entry.Value or entry.Key
-
-                    }
-
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"無法處理檔案！\n原因：{ex.Message}", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
+            
+            ImageDataFetchProc();
 
         }
 
@@ -55,5 +126,83 @@ namespace MetadataFinder
         {
             FocalSelector.SelectedIndex = 0;
         }
+
+        private void RefreshButton_Click(object sender, EventArgs e)
+        {
+            ImageDataFetchProc();
+        }
+
+        private void ExportTXTButton_Click(object sender, EventArgs e)
+        {
+            saveFileDialog.Filter = @"純文字檔 (*.txt)|*.txt";
+
+            if (saveFileDialog.ShowDialog() != DialogResult.OK) return;
+
+            File.WriteAllText(saveFileDialog.FileName, OutputBox.Text);
+
+        }
+
+        private void ExportWordButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("未完成，敬請期待");
+
+            //saveFileDialog.Filter = @"文件檔(*.docx) | *.docx";
+
+            //if (saveFileDialog.ShowDialog() != DialogResult.OK) return;
+
+            //var saveFileDir = saveFileDialog.FileName;
+
+            //using (var wordDocument = WordprocessingDocument.Create(saveFileDir, WordprocessingDocumentType.Document))
+            //{
+            //    var mainPart = wordDocument.AddMainDocumentPart();
+            //    mainPart.Document = new Document();
+            //    mainPart.Document.AppendChild(new Body());
+
+            //    var table = new Table();
+
+            //    var tp = new TableProperties(
+            //        //指定田字形六條線的樣式及線寬
+            //        new TableBorders(
+            //            //Size 單位為 1/8 點 [註]
+            //            new TopBorder() { Val = new EnumValue<BorderValues>(BorderValues.Single), Size = 8 },
+            //            new BottomBorder() { Val = new EnumValue<BorderValues>(BorderValues.Single), Size = 8 },
+            //            new LeftBorder() { Val = new EnumValue<BorderValues>(BorderValues.Single), Size = 8 },
+            //            new RightBorder() { Val = new EnumValue<BorderValues>(BorderValues.Single), Size = 8 },
+            //            new InsideHorizontalBorder() { Val = new EnumValue<BorderValues>(BorderValues.Single), Size = 8 },
+            //            new InsideVerticalBorder() { Val = new EnumValue<BorderValues>(BorderValues.Single), Size = 8 }
+            //        )
+            //    );
+            //    tp.AppendChild(new TableWidth() { Width = "100%" });
+            //    table.AppendChild(tp); 
+            //    var row = new TableRow();
+
+            //    for (var i = 0; i < ColumNumPickup.Value; i++)
+            //    {
+            //        var cell = new TableCell();
+            //    }
+
+
+            //    var cell1 = new TableCell();
+            //    var cell2 = new TableCell();
+
+
+            //    //cell1.Append(new Paragraph(new Run(new Text("123"))));
+            //    //cell2.Append(new Paragraph(new Run(new Text("456"))));
+
+            //    row.Append(cell1);
+            //    row.Append(cell2);
+
+
+            //    table.Append(row);
+
+
+            //    mainPart.Document.Body.AppendChild(table);
+
+
+            //    mainPart.Document.Save();
+            //}
+
+        }
     }
+
 }
