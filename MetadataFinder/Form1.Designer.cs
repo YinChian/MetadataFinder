b36105970fdf6b20650522080978036f6bab4abd
YinChian <82175095+YinChian@users.noreply.github.com>
2024-04-19 23:18:07 +0800

    整理metaData存放的方式 整理Form Layout

--- MetadataFinder/Form1.Designer.cs
+++ MetadataFinder/Form1.Designer.cs
@@ -34,18 +34,30 @@
             OutputBox = new TextBox();
             folderBrowserDialog = new FolderBrowserDialog();
             FocalSelector = new ComboBox();
-            RefreshButton = new Button();
+            ReadButton = new Button();
             saveFileDialog = new SaveFileDialog();
             ColumNumPickup = new NumericUpDown();
             label1 = new Label();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            groupBox3 = new GroupBox();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            FocalNumericSelector = new NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)ColumNumPickup).BeginInit();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)FocalNumericSelector).BeginInit();
             SuspendLayout();
             // 
             // OpenFileButton
             // 
-            OpenFileButton.Location = new Point(12, 12);
+            OpenFileButton.Location = new Point(15, 15);
+            OpenFileButton.Margin = new Padding(4);
             OpenFileButton.Name = "OpenFileButton";
-            OpenFileButton.Size = new Size(340, 23);
+            OpenFileButton.Size = new Size(437, 29);
             OpenFileButton.TabIndex = 0;
             OpenFileButton.Text = "開啟資料夾...";
             OpenFileButton.UseVisualStyleBackColor = true;
@@ -54,9 +66,10 @@
             // ExportTXTButton
             // 
             ExportTXTButton.Enabled = false;
-            ExportTXTButton.Location = new Point(225, 415);
+            ExportTXTButton.Location = new Point(7, 23);
+            ExportTXTButton.Margin = new Padding(4);
             ExportTXTButton.Name = "ExportTXTButton";
-            ExportTXTButton.Size = new Size(260, 23);
+            ExportTXTButton.Size = new Size(99, 29);
             ExportTXTButton.TabIndex = 1;
             ExportTXTButton.Text = "轉出文字檔";
             ExportTXTButton.UseVisualStyleBackColor = true;
@@ -65,11 +78,12 @@
             // ExportWordButton
             // 
             ExportWordButton.Enabled = false;
-            ExportWordButton.Location = new Point(225, 444);
+            ExportWordButton.Location = new Point(7, 24);
+            ExportWordButton.Margin = new Padding(4);
             ExportWordButton.Name = "ExportWordButton";
-            ExportWordButton.Size = new Size(127, 23);
+            ExportWordButton.Size = new Size(135, 29);
             ExportWordButton.TabIndex = 2;
-            ExportWordButton.Text = "轉出Word檔";
+            ExportWordButton.Text = "轉出Word檔表格";
             ExportWordButton.UseVisualStyleBackColor = true;
             ExportWordButton.Click += ExportWordButton_Click;
             // 
@@ -77,12 +91,13 @@
             // 
             OutputBox.AcceptsReturn = true;
             OutputBox.BorderStyle = BorderStyle.FixedSingle;
-            OutputBox.Location = new Point(12, 41);
+            OutputBox.Location = new Point(15, 52);
+            OutputBox.Margin = new Padding(4);
             OutputBox.Multiline = true;
             OutputBox.Name = "OutputBox";
             OutputBox.ReadOnly = true;
             OutputBox.ScrollBars = ScrollBars.Both;
-            OutputBox.Size = new Size(473, 368);
+            OutputBox.Size = new Size(608, 466);
             OutputBox.TabIndex = 3;
             // 
             // FocalSelector
@@ -91,60 +106,139 @@
             FocalSelector.FlatStyle = FlatStyle.System;
             FocalSelector.FormattingEnabled = true;
             FocalSelector.Items.AddRange(new object[] { "全片幅 (不使用等校焦段)", "Nikon APS-C 片幅 (1.5倍)", "Nikon CX 片幅 (1.7倍)", "Canon APS-H 片幅 (1.3倍)", "Canon APS-C 片幅 (1.6倍)", "Sony APS-C 片幅 (1.5倍)" });
-            FocalSelector.Location = new Point(10, 417);
+            FocalSelector.Location = new Point(118, 25);
+            FocalSelector.Margin = new Padding(4);
             FocalSelector.Name = "FocalSelector";
-            FocalSelector.Size = new Size(209, 23);
+            FocalSelector.Size = new Size(206, 27);
             FocalSelector.TabIndex = 5;
             // 
-            // RefreshButton
+            // ReadButton
             // 
-            RefreshButton.Enabled = false;
-            RefreshButton.Location = new Point(358, 12);
-            RefreshButton.Name = "RefreshButton";
-            RefreshButton.Size = new Size(127, 23);
-            RefreshButton.TabIndex = 6;
-            RefreshButton.Text = "重新讀取";
-            RefreshButton.UseVisualStyleBackColor = true;
-            RefreshButton.Click += RefreshButton_Click;
+            ReadButton.Enabled = false;
+            ReadButton.Location = new Point(460, 15);
+            ReadButton.Margin = new Padding(4);
+            ReadButton.Name = "ReadButton";
+            ReadButton.Size = new Size(163, 29);
+            ReadButton.TabIndex = 6;
+            ReadButton.Text = "讀取";
+            ReadButton.UseVisualStyleBackColor = true;
+            ReadButton.Click += RefreshButton_Click;
             // 
             // ColumNumPickup
             // 
-            ColumNumPickup.Location = new Point(505, 566);
+            ColumNumPickup.Location = new Point(52, 67);
+            ColumNumPickup.Margin = new Padding(4);
             ColumNumPickup.Maximum = new decimal(new int[] { 5, 0, 0, 0 });
             ColumNumPickup.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
             ColumNumPickup.Name = "ColumNumPickup";
-            ColumNumPickup.Size = new Size(92, 23);
+            ColumNumPickup.Size = new Size(90, 27);
             ColumNumPickup.TabIndex = 7;
             ColumNumPickup.Value = new decimal(new int[] { 2, 0, 0, 0 });
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(358, 448);
-            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Location = new Point(7, 72);
             label1.Name = "label1";
-            label1.Size = new Size(31, 15);
+            label1.Size = new Size(39, 19);
             label1.TabIndex = 8;
             label1.Text = "欄數";
             // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(ExportTXTButton);
+            groupBox1.Location = new Point(352, 527);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(113, 104);
+            groupBox1.TabIndex = 9;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "文字檔輸出";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(ExportWordButton);
+            groupBox2.Controls.Add(ColumNumPickup);
+            groupBox2.Controls.Add(label1);
+            groupBox2.Location = new Point(471, 525);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(152, 106);
+            groupBox2.TabIndex = 10;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Word檔輸出";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(radioButton2);
+            groupBox3.Controls.Add(radioButton1);
+            groupBox3.Controls.Add(FocalNumericSelector);
+            groupBox3.Controls.Add(FocalSelector);
+            groupBox3.Location = new Point(15, 527);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(331, 104);
+            groupBox3.TabIndex = 11;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "焦段倍率設定";
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(6, 65);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(90, 23);
+            radioButton2.TabIndex = 8;
+            radioButton2.Text = "手動輸入";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Checked = true;
+            radioButton1.Location = new Point(6, 28);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(105, 23);
+            radioButton1.TabIndex = 7;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "使用預設值";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // FocalNumericSelector
+            // 
+            FocalNumericSelector.DecimalPlaces = 1;
+            FocalNumericSelector.Enabled = false;
+            FocalNumericSelector.Increment = new decimal(new int[] { 1, 0, 0, 65536 });
+            FocalNumericSelector.Location = new Point(119, 65);
+            FocalNumericSelector.Maximum = new decimal(new int[] { 10, 0, 0, 0 });
+            FocalNumericSelector.Minimum = new decimal(new int[] { 1, 0, 0, 65536 });
+            FocalNumericSelector.Name = "FocalNumericSelector";
+            FocalNumericSelector.Size = new Size(205, 27);
+            FocalNumericSelector.TabIndex = 6;
+            FocalNumericSelector.Value = new decimal(new int[] { 11, 0, 0, 65536 });
+            // 
             // MainWindow
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(9F, 19F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(497, 475);
-            Controls.Add(label1);
-            Controls.Add(ColumNumPickup);
-            Controls.Add(RefreshButton);
-            Controls.Add(FocalSelector);
+            ClientSize = new Size(639, 648);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(ReadButton);
             Controls.Add(OutputBox);
-            Controls.Add(ExportWordButton);
-            Controls.Add(ExportTXTButton);
             Controls.Add(OpenFileButton);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(4);
             MaximizeBox = false;
             Name = "MainWindow";
-            Text = "Form1";
+            Text = "MetadataGenerator";
             Load += MainWindow_Load;
             ((System.ComponentModel.ISupportInitialize)ColumNumPickup).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)FocalNumericSelector).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -157,9 +251,16 @@
         private TextBox OutputBox;
         private FolderBrowserDialog folderBrowserDialog;
         private ComboBox FocalSelector;
-        private Button RefreshButton;
+        private Button ReadButton;
         private SaveFileDialog saveFileDialog;
         private NumericUpDown ColumNumPickup;
         private Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private GroupBox groupBox3;
+        private RadioButton radioButton1;
+        private NumericUpDown FocalNumericSelector;
+        private RadioButton radioButton2;
     }
 }
--- MetadataFinder/Form1.cs
+++ MetadataFinder/Form1.cs
@@ -1,48 +1,51 @@
+using System.Net.Http.Headers;
 using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
 using System.Text;
+using DocumentFormat.OpenXml.Vml;
 
 
 namespace MetadataFinder
 {
     public partial class MainWindow : Form
     {
-        private string _imageLoadingDir = "";
-        //private bool _isTouched;
-        //private int _fileCount;
-        private readonly List<string> _metaData = new();
 
-        private void ImageDataFetchProc()
+        private string _imageDir = "";
+        private List<Dictionary<string, string>> _imageMetaList = new();
+
+
+        private List<Dictionary<string, string>> ImageDataFetchProc(string imageLoadingDir)
         {
-            OutputBox.Clear();
+            var metaDataList = new List<Dictionary<string, string>>();
 
-            var imageFiles = Directory.GetFiles(_imageLoadingDir, "*.jpg");
+            _imageMetaList.Clear();
 
-            if (imageFiles.Length > 50)
+
+            if (FocalSelector.SelectedIndex == 0)
             {
-                var responseResult = MessageBox.Show(
-                    @"您正在開啟大量圖片檔案！這樣做我無法保證程式的穩定性，您確定要繼續嗎？", 
-                    @"大量開啟警告", 
-                    MessageBoxButtons.YesNo, 
-                    MessageBoxIcon.Warning,
-                    MessageBoxDefaultButton.Button2
+                MessageBox.Show(
+                    $@"您選擇了全片幅輸出，將不會顯示等效焦段！{Environment.NewLine}如果這不是您想要的，可以在重新選好焦段後按下重新整理按鈕。",
+                    @"確定齁",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information
                     );
-
-                if (responseResult == DialogResult.No) return;
             }
+
+            var imageFiles = Directory.GetFiles(imageLoadingDir, "*.jpg");
+
+
 
             foreach (var imagePath in imageFiles)
             {
+                var metaData = new Dictionary<string, string>
+                {
+                    ["檔案名稱"] = imagePath
+                };
 
-                OutputBox.Text += imagePath;
-                OutputBox.Text += Environment.NewLine;
-
-                var metaData = "";
                 try
                 {
                     var image = Image.FromFile(imagePath);
-                    //statusStrip.Text = @"處理中";
                     foreach (var propertyItem in image.PropertyItems)
                     {
                         switch (propertyItem.Id)
@@ -50,25 +53,31 @@ namespace MetadataFinder
                             // 時間
                             case 0x0132:
                                 {
-                                    var dateTaken = Encoding.UTF8.GetString(propertyItem.Value).Replace("\0", string.Empty);
-                                    metaData += $@"拍攝時間: {dateTaken}";
-                                    metaData += Environment.NewLine;
+                                    metaData["拍攝時間"] = Encoding.UTF8.GetString(propertyItem.Value)
+                                        .Replace("\0", string.Empty);
                                     break;
                                 }
+
                             // 焦段
                             case 0x920A:
                                 {
                                     var focalLength = BitConverter.ToInt32(propertyItem.Value, 0);
+                                    
+                                    metaData["焦段"] = $@"{focalLength} mm";
 
-                                    metaData += $@"焦段: {focalLength} mm";
-
-                                    if (FocalSelector.SelectedIndex != 0)
+                                    if (radioButton1.Checked && FocalSelector.SelectedIndex != 0)
                                     {
                                         double[] focal = { 1.5, 1.7, 1.3, 1.6, 1.5 };
-                                        metaData += $@" (等校焦段: {Math.Round(focalLength * focal[FocalSelector.SelectedIndex - 1], 1)})";
+                                        //metaData["等效焦段"] =
+                                        //    $@"{Math.Round(focalLength * focal[FocalSelector.SelectedIndex - 1], 1)} mm";
+                                        metaData["焦段"] +=
+                                            $@" (等效焦段: {Math.Round(focalLength * focal[FocalSelector.SelectedIndex - 1], 1)} mm)";
                                     }
-
-                                    metaData += Environment.NewLine;
+                                    else if (radioButton2.Checked && Math.Abs((double)FocalNumericSelector.Value - 1.0) > 0.01)
+                                    {
+                                        metaData["焦段"] +=
+                                            $@" (等效焦段: {Math.Round(focalLength * FocalNumericSelector.Value, 1)} mm)";
+                                    }
                                     break;
                                 }
 
@@ -76,9 +85,8 @@ namespace MetadataFinder
                             case 0x8827:
                                 {
                                     var iso = BitConverter.ToUInt16(propertyItem.Value, 0);
+                                    metaData["ISO"] = $@"{iso}";
 
-                                    metaData += $@"ISO: {iso}";
-                                    metaData += Environment.NewLine;
                                     break;
                                 }
 
@@ -90,13 +98,12 @@ namespace MetadataFinder
 
                                     if (denominator is 1 or 10)
                                     {
-                                        metaData += $@"曝光時間: {((double)numerator / denominator)} 秒";
+                                        metaData["曝光時間"] = @$"{(double)numerator / denominator} 秒";
                                     }
                                     else
                                     {
-                                        metaData += $@"曝光時間: {numerator}/{denominator} 秒";
+                                        metaData["曝光時間"] = $@"{numerator}/{denominator} 秒";
                                     }
-                                    metaData += Environment.NewLine;
                                     break;
                                 }
 
@@ -106,28 +113,36 @@ namespace MetadataFinder
                                     var numerator = BitConverter.ToUInt32(propertyItem.Value, 0);
                                     var denominator = BitConverter.ToUInt32(propertyItem.Value, 4);
                                     var fStop = (double)numerator / denominator;
-                                    metaData += $@"光圈: f/{fStop}";
-                                    metaData += Environment.NewLine;
+                                    metaData["光圈"] = $@"光圈: f/{fStop}";
+
                                     break;
                                 }
                         }
                     }
-
-                    _metaData.Add(metaData);
-                    OutputBox.Text += metaData;
-                    OutputBox.Text += Environment.NewLine;
-                    //_fileCount++;
-
-                    _isTouched = true;
-                    ExportTXTButton.Enabled = true;
-                    ExportWordButton.Enabled = true;
+                    metaDataList.Add(metaData);
                 }
                 catch (Exception ex)
                 {
                     MessageBox.Show($@"無法處理檔案！原因：{ex.Message}", @"錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
+            return metaDataList;
         }
+
+        private void UpdateView(List<Dictionary<string, string>> imageMetaDataList)
+        {
+            OutputBox.Text = "";
+            foreach (var metaData in imageMetaDataList)
+            {
+                foreach (var meta in metaData)
+                {
+                    OutputBox.Text += $@"{meta.Key}: {meta.Value}{Environment.NewLine}";
+                }
+
+                OutputBox.Text += Environment.NewLine;
+            }
+        }
+
 
         public MainWindow()
         {
@@ -137,29 +152,63 @@ namespace MetadataFinder
 
         private void OpenFileButton_Click(object sender, EventArgs e)
         {
-            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            if (folderBrowserDialog.ShowDialog() != DialogResult.OK) return;
+
+            _imageDir = folderBrowserDialog.SelectedPath;
+
+            var fileCount = Directory.GetFiles(_imageDir, "*.jpg").Length;
+
+            switch (fileCount)
             {
-                _imageLoadingDir = folderBrowserDialog.SelectedPath;
+                case > 50:
+                    MessageBox.Show(
+                    @"您正在開啟大量圖片檔案！這樣做我無法保證程式的穩定性。建議您分裝圖片。",
+                    @"大量開啟警告",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                    );
+                    break;
+
+                case 0:
+                    MessageBox.Show(
+                        @"找不到資料夾中的圖片檔案！"
+                    );
+                    break;
             }
-            else return;
-            
-            ImageDataFetchProc();
 
-        }
-
-        private void MainWindow_Load(object sender, EventArgs e)
-        {
-            FocalSelector.SelectedIndex = 0;
+            OutputBox.Text = @$"已載入{fileCount}張圖片，按下右上方的讀取以讀取您的檔案。";
+            ExportTXTButton.Enabled = false;
+            ExportWordButton.Enabled = false;
+            ReadButton.Enabled = true;
+            ReadButton.Text = @"讀取";
+            ReadButton.Focus();
         }
 
         private void RefreshButton_Click(object sender, EventArgs e)
         {
-            ImageDataFetchProc();
+            if (radioButton1.Checked && FocalSelector.SelectedIndex == -1)
+            {
+                MessageBox.Show(
+                    @"請選擇左下方的焦段！",
+                    @"錯誤",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+                return;
+            }
+
+            _imageMetaList = ImageDataFetchProc(_imageDir);
+            UpdateView(_imageMetaList);
+            ExportTXTButton.Enabled = true;
+            ExportWordButton.Enabled = true;
+            ReadButton.Text = @"重新讀取";
         }
 
         private void ExportTXTButton_Click(object sender, EventArgs e)
         {
+            saveFileDialog.Reset();
             saveFileDialog.Filter = @"純文字檔 (*.txt)|*.txt";
+
 
             if (saveFileDialog.ShowDialog() != DialogResult.OK) return;
 
@@ -169,13 +218,25 @@ namespace MetadataFinder
 
         private void ExportWordButton_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(@"未完成，敬請期待");
-
+            saveFileDialog.Reset();
             saveFileDialog.Filter = @"文件檔(*.docx) | *.docx";
+
 
             if (saveFileDialog.ShowDialog() != DialogResult.OK) return;
 
             var saveFileDir = saveFileDialog.FileName;
+
+            if (_imageMetaList.Count < ColumNumPickup.Value)
+            {
+                MessageBox.Show(
+                    @"提醒您，您的圖片數量不足您設定的列數，產生的表格會有空白的列喔！", 
+                    @"提醒",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information
+                    );
+            }
+
+            var totalRow = (int)Math.Ceiling(_imageMetaList.Count / (double)ColumNumPickup.Value) * 2;
             try
             {
                 using (var wordDocument = WordprocessingDocument.Create(saveFileDir, WordprocessingDocumentType.Document))
@@ -198,8 +259,6 @@ namespace MetadataFinder
                     tp.AppendChild(new TableWidth() { Width = "100%" });
                     table.AppendChild(tp);
 
-                    var totalRow = (int)Math.Ceiling(_metaData.Count / (double)ColumNumPickup.Value) * 2;
-
                     for (var i = 0; i < totalRow; i++)
                     {
                         var row = new TableRow();
@@ -207,7 +266,7 @@ namespace MetadataFinder
                         {
                             var cell = new TableCell();
                             var photoIndex = i / 2 * (int)ColumNumPickup.Value + j;
-                            if (photoIndex < _metaData.Count)
+                            if (photoIndex < _imageMetaList.Count)
                             {
                                 if (i % 2 == 0)
                                 {
@@ -223,14 +282,11 @@ namespace MetadataFinder
                                 else
                                 {
                                     // Put description
-
                                     var run = new Run();
-                                    var lines = _metaData[photoIndex].Split(Environment.NewLine);
-                                    run.AppendChild(new Text(lines[0]));
-                                    for (var ii = 1; ii < lines.Length - 1; ii++)
+                                    foreach (var meta in _imageMetaList[photoIndex].Where(meta => meta.Key is not (@"時間" or @"檔案名稱")))
                                     {
-                                        run.AppendChild(new Break());
-                                        run.AppendChild(new Text(lines[ii]));
+                                        run.AppendChild(new Text($@"{meta.Key}: {meta.Value}"));
+                                        if (meta.Key != _imageMetaList[photoIndex].Last().Key) run.AppendChild(new Break());
                                     }
                                     cell.Append(new Paragraph(run));
                                 }
@@ -255,8 +311,25 @@ namespace MetadataFinder
             {
                 MessageBox.Show(@"請先將輸出文件關閉！", @"錯誤", MessageBoxButtons.OK, MessageBoxIcon.Hand);
             }
-            
+        }
 
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                FocalSelector.Enabled = true;
+                FocalNumericSelector.Enabled = false;
+            }
+            else
+            {
+                FocalSelector.Enabled = false;
+                FocalNumericSelector.Enabled = true;
+            }
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            FocalSelector.SelectedIndex = 0;
         }
     }
 
